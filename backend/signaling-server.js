2d24383cfe379cc61574f5e3e32d35e46cc5484e
pon <ioann.eryomenko@gmail.com>
2025-08-06 23:35:45 +0300

    clean up wip

--- backend/signaling-server.js
+++ backend/signaling-server.js
@@ -22,8 +22,6 @@ const io = socketIo(server, {
     },
 });
 
-let rooms = {};
-let socketToRoom = {};
 const WATCHROOM = "watchroom";
 
 io.on("connection", socket => {
@@ -37,51 +35,4 @@ io.on("connection", socket => {
         console.log("payerEvent: ", type, socket.id);
     });
 
-    socket.on("join", data => {
-        // let a new user join to the room
-        const roomId = data.room;
-        socket.join(roomId);
-        socketToRoom[socket.id] = roomId;
-
-        // persist the new user in the room
-        if (rooms[roomId]) {
-            rooms[roomId].push({id: socket.id, name: data.name});
-        } else {
-            rooms[roomId] = [{id: socket.id, name: data.name}];
-        }
-
-        // sends a list of joined users to a new user
-        const users = rooms[data.room].filter(user => user.id !== socket.id);
-        io.sockets.to(socket.id).emit("room_users", users);
-        console.log("[joined] room:" + data.room + " name: " + data.name);
-    });
-
-    socket.on("offer", sdp => {
-        const roomId = socketToRoom[socket.id];
-        socket.broadcast.to(roomId).emit("getOffer", sdp);
-        console.log("offer: " + socket.id);
-    });
-
-    socket.on("answer", sdp => {
-        const roomId = socketToRoom[socket.id];
-        socket.broadcast.to(roomId).emit("getAnswer", sdp);
-        console.log("answer: " + socket.id);
-    });
-
-    socket.on("candidate", candidate => {
-        const roomId = socketToRoom[socket.id];
-        socket.broadcast.to(roomId).emit("getCandidate", candidate);
-        console.log("candidate: " + socket.id);
-    });
-
-    socket.on("disconnect", () => {
-        const roomId = socketToRoom[socket.id];
-        let room = rooms[roomId];
-        if (room) {
-            room = room.filter(user => user.id !== socket.id);
-            rooms[roomId] = room;
-        }
-        socket.broadcast.to(room).emit("user_exit", {id: socket.id});
-        console.log(`[${socketToRoom[socket.id]}]: ${socket.id} exit`);
-    });
 });
--- main.js
+++ main.js
@@ -11,69 +11,20 @@ const socket = io('http://localhost:3000', {
     withCredentials: true
 });
 
+socket.on("getPlayerEvent", handleEvent);
+
 console.log(player);
+console.log(`Running dev: ${import.meta.env.DEV}`);
 
-const servers = {
-  iceServers: [
-    {
-      urls: ['stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302'],
-    },
-  ],
-  iceCandidatePoolSize: 10,
-};
-
-// Global State
-const peerConnection = new RTCPeerConnection(servers);
-let localStream = null;
-let remoteStream = null;
-let eventsChannel = null;
-
-// HTML elements
-const localVideo = document.getElementById('webcamVideo');
-const remoteVideo = document.getElementById('remoteVideo');
-const hangupButton = document.getElementById('hangupButton');
-const chatButton = document.getElementById('chatButton');
-const sendButton = document.getElementById('send');
-
-remoteStream = new MediaStream();
-remoteVideo.srcObject = remoteStream;
-
-// 0. Setup my asshole
-
-document.addEventListener('keydown', (event) => {
-  console.log(`Key pressed: ${event.key}`);
-});
-
-// 1. Setup media sources
-
-// event channel creation
-chatButton.onclick = () => {
-  eventsChannel = peerConnection.createDataChannel('events', {
-    ordered: false
-  });
-
-  console.log("Event channel init attempt...");
-
-  eventsChannel.onopen = handleDataChannelOpen;
-  eventsChannel.onmessage = handleEvent;
-}
-
-sendButton.onclick = () => {
-  emitEvent("dicks", {one: "two"});
-}
 
 // Emitting an event
 function emitEvent(type, payload={}) {
-  // if (eventsChannel) {
-  //   eventsChannel.send(JSON.stringify({ type, payload }));
-  // }
-
   if (Date.now() - lastEventMillis < eventTimeoutPeriod) {
       console.log("Emit event aborted for: ", type);
       return;
     }
 
-    payload['timestamp'] = Date.now();
+    payload.timestamp = Date.now();
 
     console.log("Sending event: ", type);
     socket.emit("playerEvent", type, payload, (present) => {
@@ -85,8 +36,7 @@ let lastEventMillis = Date.now();
 const eventTimeoutPeriod = 100;
 
 function handleEvent(type, payload) {
-  // let { type, payload } = JSON.parse(event.data);
-  const transportTime = Date.now() - payload['timestamp'];
+  const transportTime = Date.now() - payload.timestamp;
   console.log(`Received event: ${type} in ${transportTime} ms, `, payload);
   lastEventMillis = Date.now();
 
@@ -98,22 +48,22 @@ function handleEvent(type, payload) {
       player.play();
       break;
     case 'seeked':
-      const wasPlaying = !player.paused();
+      // const wasPlaying = !player.paused();
       
-      if (wasPlaying) {
-        player.pause();
-      }
+      // if (wasPlaying) {
+      //   player.pause();
+      // }
 
-      player.currentTime(payload['time']);
+      player.currentTime(payload.time);
 
-      if (wasPlaying) {
-        player.play();
-      }
+      // if (wasPlaying) {
+      //   player.play();
+      // }
 
       break;
     case 'sanityCheck':
       const localTime = player.currentTime();
-      const remoteTime = payload['currentTime'] + transportTime/1000;
+      const remoteTime = payload.currentTime + transportTime/1000;
       const timeDiff = localTime - remoteTime;
       console.log(`${Math.abs(timeDiff) > 2 ? '!!!!!!!!!':''} TimeDiff: ${timeDiff}`);
       break;
@@ -136,121 +86,3 @@ function handleDataChannelOpen() {
 player.on('play', () => emitEvent('play'));
 player.on('pause', () => emitEvent('pause'));
 player.on('seeked', () => emitEvent('seeked', {'time': player.currentTime()}));
-
-
-
-// Socket.io-client handling webrtc connection
-socket.on('connect', () => {
-  console.log('Hello, successfully connected to the signaling server!');
-});
-
-socket.on("room_users", (users) => {
-  console.log("Current room users: ", users);
-  if (users.length) {
-    createOffer();
-  }
-});
-
-socket.on("getOffer", (sdp) => {
-  // console.log("get offer:", sdp);
-  console.log("Received offer");
-  createAnswer(sdp);
-});
-
-socket.on("getAnswer", (sdp) => {
-  // console.log("Received answer: ", sdp);
-  console.log("Received answer: ");
-  peerConnection.setRemoteDescription(sdp);
-});
-
-socket.on("getCandidate", (candidate) => {
-  peerConnection.addIceCandidate(new RTCIceCandidate(candidate)).then(() => {
-    console.log("Added new remote ICE canditate: ", candidate.candidate);
-  });
-});
-
-socket.on("getPlayerEvent", handleEvent);
-
-const createOffer = () => {
-    console.log("Creating offer...");
-    peerConnection
-        .createOffer()
-        .then(sdp => {
-            peerConnection.setLocalDescription(sdp);
-            socket.emit("offer", sdp);
-        })
-        .catch(error => {
-            console.log(error);
-        });
-};
-
-const createAnswer = (sdp) => {
-    peerConnection.setRemoteDescription(sdp).then(() => {
-        console.log("Remote description set");
-        peerConnection
-            .createAnswer()
-            .then(sdp1 => {
-                console.log("Creating answer...");
-                peerConnection.setLocalDescription(sdp1);
-                socket.emit("answer", sdp1);
-            })
-            .catch(error => {
-                console.log(error);
-            });
-    });
-};
-
-async function init(e) {
-    console.log("init");
-    try {
-        navigator.mediaDevices
-            .getDisplayMedia({
-                video: true,
-                audio: true,
-            })
-            .then(stream => {
-                localVideo.srcObject = stream;
-
-                stream.getTracks().forEach(track => {
-                    peerConnection.addTrack(track, stream);
-                });
-                peerConnection.onicecandidate = e => {
-                    if (e.candidate) {
-                        console.log("New local ICE: ", e.candidate.candidate);
-                        socket.emit("candidate", e.candidate);
-                    }
-                };
-                peerConnection.oniceconnectionstatechange = e => {
-                    console.log("ICE state change: ", e.target);
-                };
-
-                peerConnection.ontrack = (event) => {
-                  event.streams[0].getTracks().forEach((track) => {
-                    remoteStream.addTrack(track);
-                  });
-                };
-
-                // channel isn't opening for some reason
-                peerConnection.ondatachannel = event => {
-                  eventsChannel = event.channel;
-                  eventsChannel.onopen    = handleDataChannelOpen;
-                  eventsChannel.onmessage = handleEvent;
-                };
-
-                socket.emit("join", {
-                    room: "1234",
-                    name: "skydoves@getstream.io",
-                });
-            })
-            .catch(error => {
-                console.log(`getUserMedia error: ${error}`);
-            });
-    } catch (e) {
-        console.log(e);
-    }
-}
-
-document.getElementById('join').addEventListener('click', e => init(e));
-
-
-
